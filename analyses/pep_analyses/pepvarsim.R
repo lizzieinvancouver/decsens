bde57cfb26f38ada286d52a7f29e7ce7a736dc50
cchambe12 <cchamberlain@g.harvard.edu>
2020-01-13 14:29:37 -0500

    copying data and scripts from ospree over

---
+++ analyses/pep_analyses/pepvarsim.R
@@ -0,0 +1,208 @@
+## Code to see if you can get lower estimated sensitivitie soley due to higher temps and non-constant monitoring... ##
+## Started 14 February 2019 ##
+## Snow and clouds from the train to Seattle ##
+## Started by Lizzie ##
+
+## NOTE: These sims are designed to contrast a BEFORE and AFTER climate change sensitivity etc. ... you would need to adjust the f(x) to do time-series-style change over time approach. But since this was the approach we were taking to the PEP725 data (i.e., look at 10 years before CC and 10 years after) I did it this way.
+
+# Updated on 18 April 2019: Added: Plots of estimated sens versus warming and plots of variance in leafout date versus warming
+
+## housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+set.seed(113)
+
+# Need to simulate data as GDD system; only really need forcing #
+
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("ailene", getwd()))>0) { 
+  setwd("~/Documents/GitHub/ospree/analyses/bb_analysis/pep_sims")
+} else if(length(grep("lizzie", getwd()))>0) {
+  setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis/pep_sims")
+} else if(length(grep("Ignacio", getwd()))>0) { 
+  setwd("~/GitHub/ospree/analyses/bb_analysis/pep_sims") 
+} else if(length(grep("catchamberlain", getwd()))>0) { 
+  setwd("~/Documents/git/ospree/analyses/bb_analysis/pep_sims")
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis/pep_sims")
+
+figpath <- "figures"
+
+source("pepvarsimfxs.R")
+
+## Below is the original loop that I wrote ... not sure if we need to keep it?
+
+if(FALSE){
+# This controls how many runs of the whole thing you do ...
+drawstotal <- 50
+draws <- c()
+
+# Big outside loop ... 
+for(j in 1:drawstotal){
+
+# Step 1: Set up years, days per year, temperatures, sampling frequency, required GDD (fstar)
+daysperyr <- 60
+yrstotal <- 40
+yrs <- rep(1:yrstotal, each=daysperyr)
+dayz <- rep(1:daysperyr, 40)
+precc <- 10
+postcc <- 12
+sigma <- 8
+fstar <- 400
+samplefreq <- 3
+
+cc <- c(rep("precc", yrstotal/2), rep("postcc", yrstotal/2))
+
+# Step 2: here we go, set up some empty vectors, then fill with random draws based on temps above
+dailytemp <- c()
+gdd <- c()
+
+for (i in 1:yrstotal){
+    if (i<(yrstotal/2+1)) {
+        dailytemphere <- rnorm(daysperyr, precc, sigma)
+        } else {
+        dailytemphere <- rnorm(daysperyr, postcc, sigma)
+        }
+    dailytemp <- c(dailytemp, dailytemphere)
+    gdd <- c(gdd, cumsum(dailytemphere))
+    }
+
+# Step 3: Make a data frame and get the mean temp per year (needed later to calculate sensitivities)
+df <- data.frame(cbind(yrs, dayz, dailytemp, gdd))
+df.meantemp <- aggregate(df["dailytemp"], df[c("yrs")], FUN=mean)
+plot(dailytemp ~ yrs, data=df.meantemp)
+
+# Step 4: Now, in a very slow, painful way I get the BB date
+df$bb.YN <- NA
+
+for (i in c(1:nrow(df))){ # This loop just makes a Yes/No vector for budburst
+    if (df$gdd[i]<fstar) {
+        df$bb.YN[i] <- "N"
+        } else {
+        df$bb.YN[i] <- "Y"
+        }
+    }
+
+# Step 5: Now we remove rows based on sampling frequency and then calculate the observed BB date
+df.sample <- df[seq(1, nrow(df), samplefreq),]
+
+bbdate <- c()
+
+for (i in 1:yrstotal){
+    subby <- subset(df.sample, yrs==i & bb.YN=="Y")
+    bbdate[i] <- subby$dayz[1]
+    }
+
+# Step 6: Whoop! Now we can caculate temperature sensitivities.
+dfcalc <- cbind(bbdate, df.meantemp, cc)
+estprecc <- lm(bbdate~dailytemp, data=subset(dfcalc, cc=="precc"))
+estpostcc <- lm(bbdate~dailytemp, data=subset(dfcalc, cc=="postcc"))
+
+diffbefore.after <- coef(estprecc)[2]-coef(estpostcc)[2]
+# negative means a decline in sensitivity AFTER climate change
+
+draws <- rbind(draws, diffbefore.after)
+}
+
+mean(draws)
+hist(draws, breaks=10)
+}
+
+# Looking at effect of sampling freq and warming
+if(FALSE){
+sim.1d.2deg <- pepvariance.sim(100, 10, 12, 1, 6, 400)
+sim.3d.2deg <- pepvariance.sim(100, 10, 12, 3, 6, 400)
+sim.7d.2deg <- pepvariance.sim(100, 10, 12, 7, 6, 400)
+# sim.20d.2deg <- pepvariance.sim(100, 10, 12, 20, 6, 400)
+sim.3d.0deg <- pepvariance.sim(100, 10, 10, 3, 6, 400)
+sim.3d.5deg <- pepvariance.sim(100, 10, 15, 3, 6, 400)
+
+breaks=20
+
+pdf(file.path(figpath, "sims.pdf"), width = 8, height = 6)
+par(mfrow=c(2,3))
+hist(sim.1d.2deg, xlab="Diff sens (before-after cc)", main="Sample daily", breaks=breaks)
+abline(v=mean(sim.1d.2deg), col="red")
+hist(sim.3d.2deg, xlab="Diff sens (before-after cc)", main="Sample every 3 days", breaks=breaks)
+abline(v=mean(sim.3d.2deg), col="red")
+hist(sim.7d.2deg, xlab="Diff sens (before-after cc)", main="Sample every 7 days", breaks=breaks)
+abline(v=mean(sim.7d.2deg), col="red")
+
+hist(sim.3d.0deg, xlab="Diff sens (before-after cc)", main="No temp change", breaks=breaks)
+abline(v=mean(sim.3d.0deg), col="red")
+hist(sim.3d.2deg, xlab="Diff sens (before-after cc)", main="Two deg change", breaks=breaks)
+abline(v=mean(sim.3d.2deg), col="red")
+hist(sim.3d.5deg, xlab="Diff sens (before-after cc)", main="Five deg change", breaks=breaks)
+abline(v=mean(sim.3d.5deg), col="red")
+dev.off()
+}
+
+# drawstotal, yrstotal (must be dividisable by 2), precctemp, postcctemp, samplefreq, sigma, fstar
+sim.1deg <- pepvariance.sim(100, 40, 10, 11, 3, 6, 400)
+sim.2deg <- pepvariance.sim(100, 40, 10, 12, 3, 6, 400)
+sim.3deg <- pepvariance.sim(100, 40, 10, 13, 3, 6, 400)
+sim.4deg <- pepvariance.sim(100, 40, 10, 14, 3, 6, 400)
+sim.5deg <- pepvariance.sim(100, 40, 10, 15, 3, 6, 400)
+
+sim.1deg$degwarm <- 1
+sim.2deg$degwarm <- 2
+sim.3deg$degwarm <- 3
+sim.4deg$degwarm <- 4
+sim.5deg$degwarm <- 5
+
+
+degwarm.runs <- rbind(sim.1deg, sim.2deg, sim.3deg, sim.4deg, sim.5deg)
+# write.csv(degwarm.runs, "output/degwarmpepsims10CFstar400.csv", row.names=FALSE)
+
+plot(postcc.sens~as.factor(degwarm), data=degwarm.runs, xlab="degree warming", ylab="temperature sensitivity")
+plot(var.lo.postcc~as.factor(degwarm), data=degwarm.runs, xlab="degree warming", ylab="var(leafout date)")
+
+# values closer to what Cat showed .... though we probably should double-check best sigma here
+sim.1deg.alt <- pepvariance.sim(45, 20, 6, 7, 3, 3, 150)
+sim.2deg.alt <- pepvariance.sim(45, 20, 6, 8, 3, 3, 150)
+sim.3deg.alt <- pepvariance.sim(45, 20, 6, 9, 3, 3, 150)
+sim.4deg.alt <- pepvariance.sim(45, 20, 6, 10, 3, 3, 150)
+
+sim.1deg.alt$degwarm <- 1
+sim.2deg.alt$degwarm <- 2
+sim.3deg.alt$degwarm <- 3
+sim.4deg.alt$degwarm <- 4
+
+degwarmalt.runs <- rbind(sim.1deg.alt, sim.2deg.alt, sim.3deg.alt, sim.4deg.alt)
+write.csv(degwarmalt.runs, "output/degwarmpepsims6CFstar150.csv", row.names=FALSE)
+
+meanhere.alt <- aggregate(degwarmalt.runs[c("diffbefore.after", "precc.sens", "postcc.sens",
+    "var.lo.precc", "var.lo.postcc")], degwarmalt.runs["degwarm"], FUN=mean)
+
+sdhere.alt <- aggregate(degwarmalt.runs[c("diffbefore.after", "precc.sens", "postcc.sens",
+    "var.lo.precc", "var.lo.postcc")], degwarmalt.runs["degwarm"], FUN=sd)
+
+# get SE for one degree 
+sdhere.alt$diffbefore.after[1]/sqrt(45)
+
+
+### Now test different sigmas before and after CC
+source("pepvarsimfxs_sigmatest.R")
+# values closer to what Cat showed .... though we probably should double-check best sigma here
+sim.1deg.alt.sigtest <- pepvariance.sim.sigmatest(45, 20, 6, 7, 3, 2, 3, 150)
+sim.2deg.alt.sigtest <- pepvariance.sim.sigmatest(45, 20, 6, 8, 3, 2, 3, 150)
+sim.3deg.alt.sigtest <- pepvariance.sim.sigmatest(45, 20, 6, 9, 3, 2, 4, 150)
+sim.4deg.alt.sigtest <- pepvariance.sim.sigmatest(45, 20, 6, 10, 3, 2, 4, 150)
+
+sim.1deg.alt.sigtest$degwarm <- 1
+sim.2deg.alt.sigtest$degwarm <- 2
+sim.3deg.alt.sigtest$degwarm <- 3
+sim.4deg.alt.sigtest$degwarm <- 4
+
+degwarmalt.runs.sigtest <- rbind(sim.1deg.alt.sigtest, sim.2deg.alt.sigtest, sim.3deg.alt.sigtest, sim.4deg.alt.sigtest)
+#write.csv(degwarmalt.runs.sigtest, "output/degwarmpepsims6CFstar150.csv", row.names=FALSE)
+
+meanhere.alt.sigtest <- aggregate(degwarmalt.runs.sigtest[c("diffbefore.after", "precc.sens", "postcc.sens",
+                                            "var.lo.precc", "var.lo.postcc")], degwarmalt.runs.sigtest["degwarm"], FUN=mean)
+
+sdhere.alt.sigtest <- aggregate(degwarmalt.runs.sigtest[c("diffbefore.after", "precc.sens", "postcc.sens",
+                                          "var.lo.precc", "var.lo.postcc")], degwarmalt.runs.sigtest["degwarm"], FUN=sd)
+
+# get SE for one degree 
+sdhere.alt$diffbefore.after[1]/sqrt(45)
+
---
+++ analyses/simmonds_slidingwin/sw_simmonds.R
@@ -0,0 +1,197 @@
+# Started 29 October 2019 by Cat
+## Using code from Emily Simmond's sliding window approach: https://github.com/emilygsimmonds/Cue_Identification
+
+## housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+library(climwin)
+library(lubridate)
+
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("ailene", getwd()))>0) { 
+  setwd("~/Documents/GitHub/ospree/analyses/bb_analysis")
+} else if(length(grep("lizzie", getwd()))>0) {
+  setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
+} else if(length(grep("Ignacio", getwd()))>0) { 
+  setwd("~/GitHub/ospree/analyses/bb_analysis") 
+} else if(length(grep("catchamberlain", getwd()))>0) { 
+  setwd("~/Documents/git/ospree/analyses/bb_analysis")
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
+
+###### FOR JUST ANALYZING RESULTS JUMP TO LINE 75 ########
+
+# Get budburst data across 45 sites for BETPEN
+# Betula puendula data from PEP (both have has GDD from 1 Jan to leafout)
+# bp has mat from March 1st to June 1st and mat.lo is 30 days before leafout (uses tg -- aka mean -- data from E-OBS)
+# bpalt is similar, but calculated uses txtm -- aka min and max (and we caculate the mean ourselves from those values) -- data from E-OBS) ... we don't use this currently 
+bp <- read.csv("PEP_climate/output/betpen_allchillsandgdds_45sites_mat_forsims.csv", header=TRUE)
+  
+bbsw <- subset(bp, select=c("year", "lo", "siteslist"))
+bbsw$bb_date <- as.Date(bbsw$lo, origin=paste0(bbsw$year, "-01-01"))
+bbsw$bb_date <- as.character(bbsw$bb_date)
+bbsw$doy95 <- bbsw$lo - 4
+
+bbsw <- subset(bbsw, select=c("year", "bb_date", "lo", "doy95", "siteslist"))
+colnames(bbsw) <- c("Year", "bb_date", "bb_mean", "doy95", "spatial")
+bbsw$bb_date <- as.character(bbsw$bb_date)
+
+bbswpre <- bbsw[(bbsw$Year<=1960),]
+bbswpost <- bbsw[(bbsw$Year>1960),]
+
+bbswtest <- bbswpre[(bbswpre$spatial==1),]
+
+### Now get the climate data for 45 sites for BETPEN (from betpen_climate_slidingwin.R)
+climatedatapre <- read.csv("pep_sims/simmonds_slidingwin/input/bp_climatedatapre.csv")
+climatedatapost <- read.csv("pep_sims/simmonds_slidingwin/input/bp_climatedatapost.csv")
+  
+source("pep_sims/simmonds_slidingwin/Run_SW.R")
+# refday = c(day, mon)
+# climate is a datafile that must include col = temp
+# datafile = biological data
+# default = absolute but can also run relative
+#run_SW <- function(absolute = TRUE, datafile, climate, refday)
+
+### Now checking Simmond's sliding window approach:
+refday <- c(31, 05) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
+datafile <- bbswpost
+climate <- climatedatapost
+climate$X <- NA ### needed in order to run... 
+
+Results_SWRpost <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
+write.csv(Results_SWRpost[[2]], file="pep_sims/simmonds_slidingwin/output/results_swapost_bp_mayref.csv")
+write.csv(Results_SWRpost[[1]], file="pep_sims/simmonds_slidingwin/output/sumstats_swapost_bp_mayref.csv")
+
+
+## Get data and parameters for prediction
+source('pep_sims/simmonds_slidingwin/Params_SW.R')
+
+# extract parameters for complete dataset
+Parameters_SWRpost <- get_params_SW(Results_SWRpost, bbswpost$bb_mean, "complete", type = "Params")
+Parameters_SWRpre <- read.csv("pep_sims/simmonds_slidingwin/output/parameters_swapre_mayref.csv")
+# SAVE
+write.csv(Parameters_SWRpost, "pep_sims/simmonds_slidingwin/output/parameters_swapost_mayref.csv", row.names=T)
+
+
+######################################################################
+#### Now we can analyze the results from the SWA: ref = May 31st #####
+######################################################################
+
+### Now let's check out the data
+swapre <- read.csv("pep_sims/simmonds_slidingwin/output/results_swapre_bp_mayref.csv")
+#swapre <- Results_SWRpre[[2]]
+swapost <- read.csv("pep_sims/simmonds_slidingwin/output/results_swapost_bp_mayref.csv")
+#swapost <- Results_SWRpost[[2]]
+
+swapre_stat <- read.csv("pep_sims/simmonds_slidingwin/output/sumstats_swapre_bp_mayref.csv")
+#swapre_stat <- Results_SWRpre[[1]]
+swapost_stat <- read.csv("pep_sims/simmonds_slidingwin/output/sumstats_swapost_bp_mayref.csv")
+#swapost_stat <- Results_SWRpost[[1]]
+
+confint(lm(yvar~climate,data=swapre))
+#                2.5 %     97.5 %
+#(Intercept) 152.960357 161.229140
+#climate      -6.022899  -4.989201
+
+confint(lm(yvar~climate, data=swapost))
+#                2.5 %     97.5 %
+#(Intercept) 124.322906 129.083060
+#climate      -3.995561  -3.181351
+
+##### Now let's try and compare the model output from the SWA to mean, sd, and variance
+mean(swapre$yvar) ### 113.8089
+mean(swapost$yvar) ## 106.3356
+
+mean(swapre$climate) ## 7.861508  
+mean(swapost$climate) ## 5.675819 
+
+sd(swapre$yvar) ## 11.58426
+sd(swapost$yvar) ## 7.950618
+
+sd(swapre$climate) ## 1.479552
+sd(swapost$climate) ## 1.403269
+
+var(swapre$yvar) ## 134.195
+var(swapost$yvar) ## 63.21232
+
+var(swapre$climate) ## 2.189074
+var(swapost$climate) ## 1.969164
+
+## Pre CC SWA results
+#   X WindowOpen WindowClose Variable     Int      EST       SE        R2
+#1 1        -22          52        1 157.0947 -5.50605 0.262991 0.4934151
+
+## Post CC SWA results
+#   WindowOpen WindowClose Variable   Int       EST        SE        R2
+#1        -34          22        1 126.703 -3.588456 0.2071495 0.3998027
+
+
+######################################################################
+## Now using code from `bb_analysis/PEP_climate/comparetopepsims.R` ##
+######################################################################
+swapre$site <- rep(1:45)
+swapost$site <- rep(1:45)
+
+swa <- rbind(swapre, swapost)
+swa$cc <- rep(c("pre", "post"), each=450)
+
+bpest <- data.frame(siteslist=numeric(), cc=character(), meanbb=numeric(), varbb=numeric(),  
+                    sdbb=numeric(), meanclim=numeric(), varclim=numeric(), sdclim=numeric(),
+                    climslope=numeric(), climslopese=numeric())
+
+
+sitez <- 1:45
+
+for(i in c(1:length(sitez))){ # i <- 1
+  subby <- subset(swa, site==sitez[i])
+  for(ccstate in c(1:2)){
+    subbycc <- subset(subby, cc==unique(swa$cc)[ccstate])
+    meanbb <- mean(subbycc$yvar, na.rm=TRUE)
+    varbb <- var(subbycc$yvar, na.rm=TRUE)
+    sdbb <- sd(subbycc$yvar, na.rm=TRUE)
+    meanclim <- mean(subbycc$climate, na.rm=TRUE)
+    varclim <- var(subbycc$climate, na.rm=TRUE)
+    sdclim <- sd(subbycc$climate, na.rm=TRUE)
+    lmclim <- lm(yvar~climate, data=subbycc)
+    lmclimse <- summary(lmclim)$coef[2,2]
+    bpestadd <- data.frame(siteslist=sitez[i], cc=unique(swa$cc)[ccstate], meanbb=meanbb, 
+                           varbb=varbb, sdbb=sdbb, meanclim=meanclim, varclim=varclim, sdclim=sdclim, 
+                           climslope=coef(lmclim)["climate"], climslopese=lmclimse)
+    bpest <- rbind(bpest, bpestadd)
+  }
+}    
+
+meanhere <- aggregate(bpest[c("meanbb", "varbb", "sdbb", "meanclim", "varclim", "sdclim", 
+                              "climslope", "climslopese")], bpest["cc"], FUN=mean)
+sdhere <- aggregate(bpest[c("meanbb", "varbb", "meanclim", "varclim","climslope")],
+                    bpest["cc"], FUN=sd)
+
+#cc            meanbb     varbb      sdbb   meanclim   varclim   sdclim climslope   climslopese
+#1 post       106.3356  45.47136  6.510241 14.49351  1.391118 1.110196 -2.048819  2.111988
+#2  pre       113.8089 132.02667 11.087655 10.81471 10.969837 2.826528 -2.683118  1.468094
+
+
+#   cc   meanbb    varbb  meanclim   varclim  climslope
+#1 post 4.759305 27.06196 1.436775  1.062947 2.605993
+#2  pre 3.926242 67.44772 1.637718 12.125168 1.923587
+
+
+
+#### Compared to Lizzie's approach to BETPEN PEP data:
+#          cc  meanmat   varmat    sdmat meanmatlo varmatlo  sdmatlo   meanlo     varlo     sdlo meanutah  meangdd  matslope matslopese
+# 1950-1960 5.365163 3.005094 1.731358  6.814883 1.363054 1.086849 113.8089 110.51111 10.25803 2246.987 68.70881 -4.534630   1.258845
+# 2000-2010 6.450939 1.251629 1.111780  6.615273 1.431603 1.152353 106.3356  46.95728  6.57374 2235.493 61.50754 -3.611025   1.579758
+
+
+
+#### But when we compare to sliding window approach: with March 1st ref window
+Parameters_SWRpre
+#     WindowOpen WindowClose Variable      Int       EST        SE        R2
+#1       -239        -238        1     136.3565 -2.084899 0.1192322 0.4043209
+
+Parameters_SWRpost
+#      WindowOpen WindowClose Variable      Int       EST        SE        R2
+#1       -184        -176        2     143.0215 -2.531195 0.1695477 0.3307278
+
+
+
