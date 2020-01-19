9a7bdbab3b502146d6d899eba67172d76dddc6f2
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2020-01-18 19:13:01 -0800

    Adding swa plots

--- analyses/pep_analyses/pepplotting.R
+++ analyses/pep_analyses/pepplotting.R
@@ -12,6 +12,7 @@ setwd("~/Documents/git/projects/treegarden/decsens/analyses")
 
 # Get data ...
 df <- read.csv("pep_analyses/output/bpenestimates_withlog.csv", header=TRUE)
+dfswa <- read.csv("pep_analyses/output/swaestimates_withlog.csv", header=TRUE)
 
 
 ##############
@@ -44,6 +45,44 @@ for(i in 1:length(unique(mean.betpen$cc))){
   pos.x <- tempdiffplot[i]
   pos.y <- mean.betpen$matslopelog[i]
   sdhere <- sd.betpen$matslopelog[i]
+  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="salmon")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
+  }
+# par(xpd=TRUE) # so I can plot legend outside
+legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Simple linear regression", "Using logged variables"),
+   cex=1, bty="n")
+dev.off()
+
+
+
+## Now with the moving windows ...
+
+mean.betpenswa <- aggregate(dfswa[c("matslope", "matslopelog", "meanmat")], dfswa["cc"], FUN=mean)
+sd.betpenswa <- aggregate(dfswa[c("matslope", "matslopelog", "meanmat")], dfswa["cc"],  FUN=sd)
+
+tempdiffswa <- mean.betpenswa$meanmat[which(mean.betpenswa$cc=="post")]-
+    mean.betpenswa$meanmat[which(mean.betpenswa$cc=="pre")]
+tempdiffplotswa <- c(0, tempdiffswa)
+
+cexhere <- 0.95
+pdf(file.path("figures/basicpepswa.pdf"), width = 6, height = 4)
+par(xpd=FALSE)
+par(mar=c(5,5,2,2))
+plot(x=NULL,y=NULL, xlim=c(-3, 1), ylim=c(-8, 1),
+     ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
+         xlab=expression(paste("Warming (", degree, "C)")), main="")
+# abline(h=0, lty=2, col="darkgrey")
+for(i in 1:length(unique(mean.betpenswa$cc))){
+  pos.x <- tempdiffplotswa[i]
+  pos.y <- mean.betpenswa$matslope[i]
+  sdhere <- sd.betpenswa$matslope[i]
+  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="darkblue")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="darkblue")
+  }
+for(i in 1:length(unique(mean.betpenswa$cc))){
+  pos.x <- tempdiffplotswa[i]
+  pos.y <- mean.betpenswa$matslopelog[i]
+  sdhere <- sd.betpenswa$matslopelog[i]
   lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="salmon")
   points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
   }
