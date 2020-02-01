e19ded3a933794c588c3e87e1060dabd4e7c83e2
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2020-01-31 16:38:56 -0800

    Updated based on new PEP data ... thanks Cat!

--- analyses/pep_analyses/pepplotting.R
+++ analyses/pep_analyses/pepplotting.R
@@ -11,120 +11,181 @@ options(stringsAsFactors = FALSE)
 setwd("~/Documents/git/projects/treegarden/decsens/analyses")
 
 # Get data ...
-df <- read.csv("pep_analyses/output/bpenestimates_withlog.csv", header=TRUE)
+df <- read.csv("pep_analyses/output/bpenestimates_withlog_1950-2010.csv", header=TRUE)
 dfswa <- read.csv("pep_analyses/output/swaestimates_withlog.csv", header=TRUE)
-df90s <- read.csv("pep_analyses/output/bpenestimates_withlog_1950_1990_2000.csv", header=TRUE)
+fs <- read.csv("pep_analyses/output/fsylestimates_withlog_1950-2010.csv", header=TRUE)
 
-fs90s <- read.csv("pep_analyses/output/fsylestimates_withlog_1950_1990_2000.csv", header=TRUE)
 
 ##############
 ## Plotting ##
 ##############
 
-mean.betpen <- aggregate(df[c("matslope", "matslopelog", "meanmat")], df["cc"], FUN=mean)
-sd.betpen <- aggregate(df[c("matslope", "matslopelog", "meanmat")], df["cc"],  FUN=sd)
+mean.betpen <- aggregate(df[c("matslope", "matslopelog", "meanmat", "varmat", "meangdd",
+    "matslopeconfint11", "matslopeconfint89", "matslopelogconfint11", "matslopelogconfint89")],
+    df["cc"], FUN=mean)
 
-mean.betpen90s <- aggregate(df90s[c("matslope", "matslopelog", "meanmat")], df90s["cc"], FUN=mean)
-sd.betpen90s <- aggregate(df90s[c("matslope", "matslopelog", "meanmat")], df90s["cc"],  FUN=sd)
+tempdiff1 <- mean.betpen$meanmat[which(mean.betpen$cc=="1970-1990")]-
+    mean.betpen$meanmat[which(mean.betpen$cc=="1950-1970")]
+tempdiff2 <- mean.betpen$meanmat[which(mean.betpen$cc=="1990-2010")]-
+    mean.betpen$meanmat[which(mean.betpen$cc=="1950-1970")]
 
-tempdiff <- mean.betpen$meanmat[which(mean.betpen$cc=="2000-2010")]-
-    mean.betpen$meanmat[which(mean.betpen$cc=="1950-1960")]
-tempdiff90s <- mean.betpen90s$meanmat[which(mean.betpen90s$cc=="1990-2000")]-
-    mean.betpen90s$meanmat[which(mean.betpen90s$cc=="1950-1960")]
-tempdiffplot <- c(0, tempdiff)
-tempdiffplot.w90s <- c(0, tempdiff90s, tempdiff)
+tempdiffplot <- c(0, tempdiff1, tempdiff2)
 
 
 cexhere <- 0.95
-pdf(file.path("figures/basicpep.pdf"), width = 6, height = 4)
+cextext <- 0.5
+pdf(file.path("figures/basicpep1950to2000.pdf"), width = 6, height = 4)
 par(xpd=FALSE)
 par(mar=c(5,5,2,2))
-plot(x=NULL,y=NULL, xlim=c(-0.5, 3), ylim=c(-6, 1),
+plot(x=NULL,y=NULL, xlim=c(-0.1, 1.5), ylim=c(-10, 1),
      ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
          xlab=expression(paste("Warming (", degree, "C)")), main="")
 # abline(h=0, lty=2, col="darkgrey")
 for(i in 1:length(unique(mean.betpen$cc))){
   pos.x <- tempdiffplot[i]
   pos.y <- mean.betpen$matslope[i]
-  sdhere <- sd.betpen$matslope[i]
-  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="darkblue")
+  ciherelo <- mean.betpen$matslopeconfint11[i]
+  cihereup <- mean.betpen$matslopeconfint89[i]
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="darkblue")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="darkblue")
+  text(pos.x + 0.1, pos.y, labels=unique(mean.betpen$cc)[i], cex=cextext, col="darkblue")
+  }
+for(i in 1:length(unique(mean.betpen$cc))){
+  pos.x <- tempdiffplot[i]
+  pos.y <- exp(mean.betpen$matslopelog[i])
+  ciherelo <- exp(mean.betpen$matslopelogconfint11[i])
+  cihereup <- exp(mean.betpen$matslopelogconfint89[i])
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="salmon")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
+  }
+legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Using raw x, y", "Using logged x, y"),
+   cex=1, bty="n")
+dev.off()
+
+
+## For Fagus sylvatica
+mean.fs <- aggregate(fs[c("matslope", "matslopelog", "meanmat", "varmat", "meangdd",
+    "matslopeconfint11", "matslopeconfint89", "matslopelogconfint11", "matslopelogconfint89")],
+    fs["cc"], FUN=mean)
+
+tempdiff1fs <- mean.fs$meanmat[which(mean.fs$cc=="1970-1990")]-
+    mean.fs$meanmat[which(mean.fs$cc=="1950-1970")]
+tempdiff2fs <- mean.fs$meanmat[which(mean.fs$cc=="1990-2010")]-
+    mean.fs$meanmat[which(mean.fs$cc=="1950-1970")]
+
+tempdiffplotfs <- c(0, tempdiff1fs, tempdiff2fs)
+
+cexhere <- 0.95
+pdf(file.path("figures/basicpep1950to2000fs.pdf"), width = 6, height = 4)
+par(xpd=FALSE)
+par(mar=c(5,5,2,2))
+plot(x=NULL,y=NULL, xlim=c(-0.1, 1.5), ylim=c(-10, 1),
+     ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
+         xlab=expression(paste("Warming (", degree, "C)")), main="")
+# abline(h=0, lty=2, col="darkgrey")
+for(i in 1:length(unique(mean.fs$cc))){
+  pos.x <- tempdiffplotfs[i]
+  pos.y <- mean.fs$matslope[i]
+  ciherelo <- mean.fs$matslopeconfint11[i]
+  cihereup <- mean.fs$matslopeconfint89[i]
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="darkblue")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="darkblue")
+  text(pos.x + 0.1, pos.y, labels=unique(mean.betpen$cc)[i], cex=cextext, col="darkblue")
+  }
+for(i in 1:length(unique(mean.fs$cc))){
+  pos.x <- tempdiffplotfs[i]
+  pos.y <- exp(mean.fs$matslopelog[i])
+  ciherelo <- exp(mean.fs$matslopelogconfint11[i])
+  cihereup <- exp(mean.fs$matslopelogconfint89[i])
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="salmon")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
+  }
+legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Using raw x, y", "Using logged x, y"),
+   cex=1, bty="n")
+dev.off()
+
+# Four panel with FagSyl
+cexhere <- 0.5
+cexhereleg <- 0.7
+pdf(file.path("figures/basicpep1950to20002spp.pdf"), width = 9, height = 6)
+par(xpd=FALSE)
+par(mfrow=c(2,2))
+par(mar=c(5,5,2,2))
+plot(x=NULL,y=NULL, xlim=c(-0.1, 1.5), ylim=c(-10, 1),
+     ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
+         xlab=expression(paste("Warming (", degree, "C)")), main="Betpen")
+abline(h=0, lty=2, col="darkgrey")
+for(i in 1:length(unique(mean.betpen$cc))){
+  pos.x <- tempdiffplot[i]
+  pos.y <- mean.betpen$matslope[i]
+  ciherelo <- mean.betpen$matslopeconfint11[i]
+  cihereup <- mean.betpen$matslopeconfint89[i]
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="darkblue")
+  text(pos.x + 0.1, pos.y, labels=unique(mean.betpen$cc)[i], cex=cextext, col="darkblue")
   points(pos.x, pos.y, cex=cexhere, pch=19, col="darkblue")
   }
 for(i in 1:length(unique(mean.betpen$cc))){
   pos.x <- tempdiffplot[i]
-  pos.y <- mean.betpen$matslopelog[i]
-  sdhere <- sd.betpen$matslopelog[i]
-  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="salmon")
+  pos.y <- exp(mean.betpen$matslopelog[i])
+  ciherelo <- exp(mean.betpen$matslopelogconfint11[i])
+  cihereup <- exp(mean.betpen$matslopelogconfint89[i])
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="salmon")
   points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
   }
-legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Simple linear regression", "Using logged variables"),
-   cex=1, bty="n")
-dev.off()
-
-# With the 90s addedd
-cexhere <- 0.95
-pdf(file.path("figures/pepbetpenw90s.pdf"), width = 6, height = 4)
-par(xpd=FALSE)
-par(mar=c(5,5,2,2))
-plot(x=NULL,y=NULL, xlim=c(-0.5, 3), ylim=c(-6, 1),
+legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Using raw x, y", "Using logged x, y"),
+   cex=cexhereleg, bty="n")
+plot(x=NULL,y=NULL, xlim=c(-0.1, 1.5), ylim=c(0.3, 1.1),
      ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
-         xlab=expression(paste("Warming (", degree, "C)")), main="")
-# abline(h=0, lty=2, col="darkgrey")
-for(i in 1:length(unique(mean.betpen90s$cc))){
-  pos.x <- tempdiffplot.w90s[i]
-  pos.y <- mean.betpen90s$matslope[i]
-  sdhere <- sd.betpen90s$matslope[i]
-  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="darkblue")
-  points(pos.x, pos.y, cex=cexhere, pch=19, col="darkblue")
-  }
-for(i in 1:length(unique(mean.betpen90s$cc))){
-  pos.x <- tempdiffplot.w90s[i]
-  pos.y <- mean.betpen90s$matslopelog[i]
-  sdhere <- sd.betpen90s$matslopelog[i]
-  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="salmon")
+         xlab=expression(paste("Warming (", degree, "C)")), main="Betpen (logged only)")
+for(i in 1:length(unique(mean.betpen$cc))){
+  pos.x <- tempdiffplot[i]
+  pos.y <- exp(mean.betpen$matslopelog[i])
+  ciherelo <- exp(mean.betpen$matslopelogconfint11[i])
+  cihereup <- exp(mean.betpen$matslopelogconfint89[i])
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="salmon")
   points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
+  text(pos.x + 0.1, pos.y, labels=unique(mean.betpen$cc)[i], cex=cextext, col="salmon")
   }
-# par(xpd=TRUE) # so I can plot legend outside
-legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Simple linear regression", "Using logged variables"),
-   cex=1, bty="n")
-dev.off()
-
-## For Fagus sylvatica
-mean.fagsyl90s <- aggregate(fs90s[c("matslope", "matslopelog", "meanmat")], fs90s["cc"], FUN=mean)
-sd.fagsyl90s <- aggregate(fs90s[c("matslope", "matslopelog", "meanmat")], fs90s["cc"],  FUN=sd)
-
-tempdiff.fs<- mean.fagsyl90s$meanmat[which(mean.fagsyl90s$cc=="2000-2010")]-
-    mean.fagsyl90s$meanmat[which(mean.fagsyl90s$cc=="1950-1960")]
-tempdiff90s.fs <- mean.fagsyl90s$meanmat[which(mean.fagsyl90s$cc=="1990-2000")]-
-    mean.fagsyl90s$meanmat[which(mean.fagsyl90s$cc=="1950-1960")]
-tempdiffplot.fs <- c(0, tempdiff90s.fs, tempdiff.fs)
-
-cexhere <- 0.95
-pdf(file.path("figures/pepfagsylw90s.pdf"), width = 6, height = 4)
-par(xpd=FALSE)
-par(mar=c(5,5,2,2))
-plot(x=NULL,y=NULL, xlim=c(-0.5, 3), ylim=c(-6, 1),
+# FagSyl 
+plot(x=NULL,y=NULL, xlim=c(-0.1, 1.5), ylim=c(-10, 1),
      ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
-         xlab=expression(paste("Warming (", degree, "C)")), main="")
-# abline(h=0, lty=2, col="darkgrey")
-for(i in 1:length(unique(mean.fagsyl90s$cc))){
-  pos.x <- tempdiffplot.fs[i]
-  pos.y <- mean.fagsyl90s$matslope[i]
-  sdhere <- sd.fagsyl90s$matslope[i]
-  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="darkblue")
+         xlab=expression(paste("Warming (", degree, "C)")), main="Fagsyl")
+abline(h=0, lty=2, col="darkgrey")
+for(i in 1:length(unique(mean.fs$cc))){
+  pos.x <- tempdiffplotfs[i]
+  pos.y <- mean.fs$matslope[i]
+  ciherelo <- mean.fs$matslopeconfint11[i]
+  cihereup <- mean.fs$matslopeconfint89[i]
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="darkblue")
   points(pos.x, pos.y, cex=cexhere, pch=19, col="darkblue")
+  text(pos.x + 0.1, pos.y, labels=unique(mean.betpen$cc)[i], cex=cextext, col="darkblue")
   }
-for(i in 1:length(unique(mean.fagsyl90s$cc))){
-  pos.x <- tempdiffplot.fs[i]
-  pos.y <- mean.fagsyl90s$matslopelog[i]
-  sdhere <- sd.fagsyl90s$matslopelog[i]
-  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col="salmon")
+for(i in 1:length(unique(mean.fs$cc))){
+  pos.x <- tempdiffplotfs[i]
+  pos.y <- exp(mean.fs$matslopelog[i])
+  ciherelo <- exp(mean.fs$matslopelogconfint11[i])
+  cihereup <- exp(mean.fs$matslopelogconfint89[i])
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="salmon")
   points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
   }
-# par(xpd=TRUE) # so I can plot legend outside
-legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Simple linear regression", "Using logged variables"),
-   cex=1, bty="n")
+legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Using raw x, y", "Using logged x, y"),
+   cex=cexhereleg, bty="n")
+plot(x=NULL,y=NULL, xlim=c(-0.1, 1.5), ylim=c(0.3, 1.1),
+     ylab=expression(paste("Estimated sensitivity (days/", degree, "C)"), sep=""),
+         xlab=expression(paste("Warming (", degree, "C)")), main="Fagsyl (logged only)")
+for(i in 1:length(unique(mean.fs$cc))){
+  pos.x <- tempdiffplotfs[i]
+  pos.y <- exp(mean.fs$matslopelog[i])
+  ciherelo <- exp(mean.fs$matslopelogconfint11[i])
+  cihereup <- exp(mean.fs$matslopelogconfint89[i])
+  lines(x=rep(pos.x, 2), y=c(ciherelo, cihereup), col="salmon")
+  points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
+  text(pos.x + 0.1, pos.y, labels=unique(mean.betpen$cc)[i], cex=cextext, col="salmon")
+  }
 dev.off()
+
+
+
 
 
 ## Now with the moving windows ...
@@ -159,6 +220,6 @@ for(i in 1:length(unique(mean.betpenswa$cc))){
   points(pos.x, pos.y, cex=cexhere, pch=19, col="salmon")
   }
 # par(xpd=TRUE) # so I can plot legend outside
-legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Simple linear regression", "Using logged variables"),
+legend("bottomright", pch=c(19, 19), col=c("darkblue", "salmon"), legend=c("Using raw x, y", "Using logged x, y"),
    cex=1, bty="n")
 dev.off()
