fe74d3426f89e05a0a257eedb32d17bb9a5388ed
cchambe12 <cchamberlain@g.harvard.edu>
2020-02-03 08:20:20 -0500

    new window for swa

--- analyses/pep_analyses/simmonds_slidingwin/bp_sw_simmonds.R
+++ analyses/pep_analyses/simmonds_slidingwin/bp_sw_simmonds.R
@@ -95,36 +95,36 @@ source("/n/wolkovich_lab/Lab/Cat/Run_SW.R")
 # datafile = biological data
 # default = absolute but can also run relative
 #run_SW <- function(absolute = TRUE, datafile, climate, refday)
-if(FALSE){
+
 ### Now checking Simmond's sliding window approach:
-refday <- c(01, 05) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
+refday <- c(01, 04) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
 datafile <- bbswpre
 climate <- climatedatapre
 climate$X <- NA ### needed in order to run... 
 
 Results_SWRpre <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
-write.csv(Results_SWRpre[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swapre_bp_mayref.csv")
-write.csv(Results_SWRpre[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swapre_bp_mayref.csv")
-}
+write.csv(Results_SWRpre[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swapre_bp_aprilref.csv")
+write.csv(Results_SWRpre[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swapre_bp_aprilref.csv")
+
 ### Now checking Simmond's sliding window approach:
-refday <- c(01, 05) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
+#refday <- c(01, 05) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
 datafile <- bbswpost
 climate <- climatedatapost
 climate$X <- NA ### needed in order to run... 
 
 Results_SWRpost <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
-write.csv(Results_SWRpost[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swapost_bp_mayref.csv")
-write.csv(Results_SWRpost[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swapost_bp_mayref.csv")
+write.csv(Results_SWRpost[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swapost_bp_aprilref.csv")
+write.csv(Results_SWRpost[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swapost_bp_aprilref.csv")
 
 ### Now checking Simmond's sliding window approach:
-refday <- c(01, 05) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
+#refday <- c(01, 04) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
 datafile <- bbswmid
 climate <- climatedatamid
 climate$X <- NA ### needed in order to run... 
 
 Results_SWRmid <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
-write.csv(Results_SWRmid[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swamid_bp_mayref.csv")
-write.csv(Results_SWRmid[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swamid_bp_mayref.csv")
+write.csv(Results_SWRmid[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swamid_bp_aprilref.csv")
+write.csv(Results_SWRmid[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swamid_bp_aprilref.csv")
 
 if(FALSE){
 ## Get data and parameters for prediction
@@ -142,14 +142,14 @@ write.csv(Parameters_SWRpost, "pep_sims/simmonds_slidingwin/output/parameters_sw
 ######################################################################
 
 ### Now let's check out the data
-swapre <- read.csv("simmonds_slidingwin/output/results_swapre_bp_mayref.csv")
+swapre <- read.csv("simmonds_slidingwin/output/results_swapre_fs_juneref.csv")
 #swapre <- Results_SWRpre[[2]]
-swapost <- read.csv("simmonds_slidingwin/output/results_swapost_bp_mayref.csv")
+swapost <- read.csv("simmonds_slidingwin/output/results_swapost_fs_juneref.csv")
 #swapost <- Results_SWRpost[[2]]
 
-swapre_stat <- read.csv("simmonds_slidingwin/output/sumstats_swapre_bp_mayref.csv")
+swapre_stat <- read.csv("simmonds_slidingwin/output/sumstats_swapre_fs_juneref.csv")
 #swapre_stat <- Results_SWRpre[[1]]
-swapost_stat <- read.csv("simmonds_slidingwin/output/sumstats_swapost_bp_mayref.csv")
+swapost_stat <- read.csv("simmonds_slidingwin/output/sumstats_swapost_fs_juneref.csv")
 #swapost_stat <- Results_SWRpost[[1]]
 
 if(FALSE){
--- analyses/pep_analyses/simmonds_slidingwin/fsfagsyl.sh
+++ analyses/pep_analyses/simmonds_slidingwin/fsfagsyl.sh
@@ -6,7 +6,7 @@
 
 #SBATCH -N 1
 
-#SBATCH -t 0-30:00:00
+#SBATCH -t 0-60:00:00
 
 #SBATCH --mem 30000
 
