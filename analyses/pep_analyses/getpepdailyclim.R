c2efdecdbd17af1957a8cca68a9618760b422c43
cchambe12 <cchamberlain@g.harvard.edu>
2020-04-20 14:16:42 -0400

    adding daily data for betpen

--- analyses/pep_analyses/getpepdailyclim.R
+++ analyses/pep_analyses/getpepdailyclim.R
@@ -19,7 +19,9 @@ require(ggplot2)
 require(lubridate)
 require(raster)
 
-setwd("~/Documents/git/projects/treegarden/decsens/analyses/pep_analyses/")
+#setwd("~/Documents/git/projects/treegarden/decsens/analyses/pep_analyses/")
+setwd("~/Documents/git/decsens/analyses/pep_analyses/")
+
 d <- read.csv("input/pep_betpen_all.csv")
 
 df<-d%>%
@@ -43,10 +45,57 @@ allpeps$num.years<-ave(allpeps$year, allpeps$PEP_ID, FUN=length)
 mostdata <- allpeps[(allpeps$num.years>=60),]
 
 lositeyear <- subset(mostdata, select=c("lo", "lat", "lon", "lat.lon", "year"))
-sitez <- subset(lositeyear, select=c("lon","lat"))
-getsitez <- sitez[!duplicated(sitez$lat),] # bad, should add lon, but works for these sites
+getsitez <- lositeyear[!duplicated(lositeyear$lat.lon),] # bad, should add lon, but works for these sites - changed column to lat.lon and then address below
+getsitez$x <- getsitez$lon
+getsitez$y  <- getsitez$lat
+Coords <- subset(getsitez, select=c(x, y))
+Coords <- na.omit(Coords)
+nsites <- length(getsitez$lat.lon)
 
-rg <- brick("/Volumes/OrangeFiend/climate/eobs/tg_ens_mean_0.25deg_reg_v20.0e.nc", sep="")
+#rg <- brick("/Volumes/OrangeFiend/climate/eobs/tg_ens_mean_0.25deg_reg_v20.0e.nc", sep="")
+rg <- brick("~/Desktop/Big Data Files/tg_0.25deg_reg_v16.0.nc")
 
-# extract data for relevant sites from Jan - May for now .. 
-goober <- raster::extract(rg, getsitez) # this seems bad
+period <- c(1951:2010)
+
+points <- SpatialPoints(Coords, proj4string = rg@crs)
+
+yearsinclim <- as.numeric(format(as.Date(names(rg),format="X%Y.%m.%d"),"%Y"))
+yearsinperiod <- which(yearsinclim%in%period)
+climsub <- subset(rg,yearsinperiod) # takes a couple of minutes
+
+## subset climate days
+monthsinclim <- as.numeric(format(as.Date(names(climsub),format="X%Y.%m.%d"),"%m"))
+dailytempmonths <- c(1:4)
+monthsindailytemps <- which(monthsinclim%in%dailytempmonths)
+dailytempssub <- subset(climsub,monthsindailytemps)
+
+
+values <- raster::extract(dailytempssub,points) ## takes many minutes!
+
+dclim <- cbind.data.frame(coordinates(points),values)
+
+require(reshape2)
+extclimdata <- melt(dclim, id.vars=c("x","y"))
+
+extclimdata <- extclimdata %>%
+  rename(long=x) %>%
+  rename(lat=y) %>%
+  rename(date=variable) %>%
+  rename(Tmean=value)
+
+dailyclimdata <- data.frame(lat=extclimdata$lat, lon=extclimdata$long, date=extclimdata$date, tmean=extclimdata$Tmean)
+
+dailyclimdata$date <- substr(dailyclimdata$date, 2,11)
+dailyclimdata$Date <- gsub("[.]", "-", dailyclimdata$date)
+
+dailyclimdata$date <- NULL
+
+dailyclimdata$year <- as.numeric(substr(dailyclimdata$Date, 0, 4))
+dailyclimdata$lat.lon <- paste(dailyclimdata$lat, dailyclimdata$lon)
+
+dailyclimdata <- dailyclimdata[!duplicated(dailyclimdata),]
+
+#fullsites45 <- left_join(full.site, mst)
+
+write.csv(dailyclimdata, file="~/Documents/git/decsens/analyses/pep_analyses/output/betpen_dailytempsJantoApr_1950to2010.csv", row.names = FALSE)
+
