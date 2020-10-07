c9c3c21f2dca2aa2d51f9e1ebdd4a31c9694c763
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2020-10-06 18:11:39 -0700

    Draft for OSPREE co-authors

--- analyses/decsensSimsFstar.R
+++ analyses/decsensSimsFstar.R
@@ -88,7 +88,7 @@ mean.sims.sm <- aggregate(dfsm[c("simplelm", "loglm")], dfsm["fstar"], FUN=mean)
 ##############
 ## Plotting ##
 ##############
-# saved ad basicsims_fstaronly_varcov
+pdf(file.path("figures/basicsims_fstaronly_varcov.pdf"), width = 10, height = 5)
 par(mfrow=c(2,4))
 plot(simplelm.trunc~fstar, data=df, xlab="thermal sum", ylab="lm sensitivity (temp until leafout)")
 plot(varx.trunc~fstar, data=df, xlab="thermal sum", ylab="var(temp until leafout)")
@@ -98,24 +98,49 @@ plot(simplelm~fstar, data=df, xlab="thermal sum", ylab="lm sensitivity (temp ove
 plot(varx~fstar, data=df, xlab="thermal sum", ylab="var(temp over window)")
 plot(covarxy~fstar, data=df, xlab="thermal sum", ylab="covar(leafout day, temp over window)")
 plot(covarlogxy~fstar, data=df, xlab="thermal sum", ylab="covar(log(leafout day), (log(temp over window))")
+dev.off()
 
 # Summarize the fstar sims
 mean.sims <- aggregate(df[c("simplelm", "loglm", "perlm", "simplelm.trunc", "loglm.trunc")], df["fstar"], FUN=mean)
 sd.sims <- aggregate(df[c("simplelm", "loglm", "perlm", "simplelm.trunc", "loglm.trunc")], df["fstar"], FUN=sd)
 
-colz <- c("blue4", "violetred4")
+colz <- c("blue4", "salmon")
 colzalpha <- adjustcolor(colz, alpha.f = 0.7)
 cexhere <- 0.75
 cexhere <- 1.2
 cextext <- 0.75
 jitterpep <- -0.04
-pdf(file.path("figures/basicsims_fstaronly.pdf"), width = 7.5, height = 5.5)
+pdf(file.path("figures/basicsims_fstaronly.pdf"), width = 7.5, height = 11)
 par(xpd=FALSE)
+par(mfrow=c(2,1))
 par(mar=c(5,5,2,2))
-plot(x=NULL,y=NULL, xlim=c(90, 310), ylim=c(-6.6, 1), yaxt="n",
+plot(x=NULL,y=NULL, xlim=c(90, 310), ylim=c(-8, 0.1), yaxt="n",
      ylab=expression(paste("Estimated sensitivity"), sep=""),
      xlab=expression(paste("Thermal sum required (", degree, "C)")), main="", cex.lab=1.2,
-     bty="l", mgp=c(1.5,.5,0), tck=-.01)
+     bty="l", mgp=c(1.5, 0.5,0), tck=-.01)
+axis(2,seq(-8,0,1),las=2)
+tempsteps <- simsnum
+tempdiffplot <- c(0,1)
+for(i in 1:tempsteps){
+  pos.x <- mean.sims$fstar[i]
+  pos.y <- mean.sims$simplelm.trunc[i]
+  sdhere <- sd.sims$simplelm.trunc[i]
+  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col=colzalpha[1])
+  points(pos.x, pos.y, cex=cexhere, pch=19, col=colzalpha[1])
+}
+for(i in 1:tempsteps){
+  pos.x <- mean.sims$fstar[i]
+  pos.y <- mean.sims$loglm.trunc[i]
+  sdhere <- sd.sims$loglm.trunc[i]
+  lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col=colzalpha[2])
+  points(pos.x, pos.y, cex=cexhere, pch=19, col=colzalpha[2])
+}
+legend("topright", pch=c(19, 19), col=c(colzalpha[2], colzalpha[1]), legend=c("Using logged variables", "Simple linear regression"),
+   cex=1, bty="n")
+plot(x=NULL,y=NULL, xlim=c(90, 310), ylim=c(-6.6, 0.1), yaxt="n",
+     ylab=expression(paste("Estimated sensitivity"), sep=""),
+     xlab=expression(paste("Thermal sum required (", degree, "C)")), main="", cex.lab=1.2,
+     bty="l", mgp=c(1.5, 0.5, 0), tck=-.01)
 axis(2,seq(-6,0,1),las=2)
 tempsteps <- simsnum
 tempdiffplot <- c(0,1)
@@ -133,6 +158,4 @@ for(i in 1:tempsteps){
   lines(x=rep(pos.x, 2), y=c(pos.y-sdhere, pos.y+sdhere), col=colzalpha[2])
   points(pos.x, pos.y, cex=cexhere, pch=19, col=colzalpha[2])
 }
-legend("topright", pch=c(19, 19), col=c(colzalpha[1], colzalpha[2]), legend=c("linear", "non-linear (logged)"),
-   cex=1, bty="n")
 dev.off()
