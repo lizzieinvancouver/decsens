b196ac6c992c7dfd228b556bf1d5850f2ff4a624
cchambe12 <cchamberlain@g.harvard.edu>
2020-12-09 19:04:31 -0500

    sliding window pre cc

--- analyses/pep_analyses/simmonds_slidingwin/bp_sw_simmonds.R
+++ analyses/pep_analyses/simmonds_slidingwin/bp_sw_simmonds.R
@@ -11,7 +11,7 @@ require(dplyr)
 require(tidyr)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
-#setwd("~/Documents/git/decsens/analyses/pep_analyses")
+setwd("~/Documents/git/decsens/analyses/pep_analyses")
 
 ###### FOR JUST ANALYZING RESULTS JUMP TO LINE 75 ########
 
@@ -19,12 +19,12 @@ require(tidyr)
 # Betula puendula data from PEP (both have has GDD from 1 Jan to leafout)
 # bp has mat from March 1st to June 1st and mat.lo is 30 days before leafout (uses tg -- aka mean -- data from E-OBS)
 # bpalt is similar, but calculated uses txtm -- aka min and max (and we caculate the mean ourselves from those values) -- data from E-OBS) ... we don't use this currently 
-#d<-read.csv("input/pep_betpen_all.csv", header=TRUE)
-d<-read.csv("/n/wolkovich_lab/Lab/Cat/pep_betpen_all.csv", header=TRUE)
+d<-read.csv("input/pep_betpen_all.csv", header=TRUE)
+#d<-read.csv("/n/wolkovich_lab/Lab/Cat/pep_betpen_all.csv", header=TRUE)
 
 df<-d%>%
   filter(BBCH==11)%>%
-  filter(YEAR>=1950 & YEAR<=2010)%>%
+  filter(YEAR>=1950 & YEAR<=2016)%>%
   dplyr::select(YEAR, DAY, BBCH, PEP_ID, LAT, LON, species)%>%
   rename(year=YEAR)%>%
   rename(lo=DAY)%>%
@@ -39,26 +39,26 @@ x<-paste(df$year, df$lo)
 df$date<-as.Date(strptime(x, format="%Y %j"))
 df$Date<- as.character(df$date)
 df$lat.long <- paste(df$lat, df$long)
-allpeps <- df[(df$year>=1951 & df$year<=2010),]
+allpeps <- df[(df$year>=1951 & df$year<=2016),]
 
 allpeps$cc<-NA
-allpeps$cc<-ifelse(allpeps$year>1950 & allpeps$year<=1970, "1950-1970", allpeps$cc)
-allpeps$cc<-ifelse(allpeps$year>1990 & allpeps$year<=2010, "1990-2010", allpeps$cc)
-allpeps$cc<-ifelse(allpeps$year>1970 & allpeps$year<=1990, "1970-1990", allpeps$cc)
-allpeps$num.years<-ave(allpeps$year, allpeps$PEP_ID, FUN=length)
-mostdata<-allpeps[(allpeps$num.years>=60),]
+allpeps$cc<-ifelse(allpeps$year>1950 & allpeps$year<=1983, "1950-1983", allpeps$cc)
+allpeps$cc<-ifelse(allpeps$year>1983 & allpeps$year<=2016, "1984-2016", allpeps$cc)
+#allpeps$cc<-ifelse(allpeps$year>1970 & allpeps$year<=1990, "1970-1990", allpeps$cc)
+allpeps$num.years<-ave(allpeps$year, allpeps$lat.long, FUN=length)
+mostdata<-allpeps[(allpeps$num.years>=65),]
 tt<-as.data.frame(table(mostdata$cc, mostdata$lat.long))
 tt<-tt[!(tt$Freq==0),]
 bestsites<-as.data.frame(table(tt$Var2))
-bestsites<-bestsites[(bestsites$Freq>2),]
+bestsites<-bestsites[(bestsites$Freq>1),]
 bestsites <- bestsites$Var1
 
 allpeps.subset<-mostdata[(mostdata$lat.long %in% bestsites),]
 
 sites<-subset(allpeps.subset, select=c(lat, long, lat.long))
 sites<-sites[!duplicated(sites$lat.long),]
-#badsites<-c("54.5 11.1", "49.7667 11.55", "47.8 11.0167") 
-#sites<-sites[!(sites$lat.long%in%badsites),]
+badsites<-c("54.5 11.1", "49.7667 11.55", "47.8 11.0167") 
+sites<-sites[!(sites$lat.long%in%badsites),]
 sites$x<-sites$long
 sites$y<-sites$lat
 nsites<-length(sites$lat.long)
@@ -78,21 +78,21 @@ bbsw <- subset(bbsw, select=c("year", "bb_date", "lo", "doy95", "siteslist"))
 colnames(bbsw) <- c("Year", "bb_date", "bb_mean", "doy95", "spatial")
 bbsw$bb_date <- as.character(bbsw$bb_date)
 
-bbswpre <- bbsw[(bbsw$Year>1950 & bbsw$Year<=1970),]
-bbswpost <- bbsw[(bbsw$Year>1990 & bbsw$Year<=2010),]
-bbswmid <- bbsw[(bbsw$Year>1970 & bbsw$Year<=1990),]
+bbswpre <- bbsw[(bbsw$Year>1950 & bbsw$Year<=1983),]
+bbswpost <- bbsw[(bbsw$Year>1983 & bbsw$Year<=2016),]
+#bbswmid <- bbsw[(bbsw$Year>1970 & bbsw$Year<=1990),]
 
 #bbswtest <- bbswpre[(bbswpre$spatial==1),]
 
 ### Now get the climate data for 45 sites for BETPEN (from betpen_climate_slidingwin.R)
-climatedatapre <- read.csv("/n/wolkovich_lab/Lab/Cat/bp_climatedatapre.csv")
-#climatedatapre <- read.csv("output/bp_climatedatapre.csv")
-climatedatapost <- read.csv("/n/wolkovich_lab/Lab/Cat/bp_climatedatapost.csv")
-#climatedatapost <- read.csv("output/bp_climatedatapost.csv")
-climatedatamid <- read.csv("/n/wolkovich_lab/Lab/Cat/bp_climatedatamid.csv")
+#climatedatapre <- read.csv("/n/wolkovich_lab/Lab/Cat/bp_climatedatapre.csv")
+climatedatapre <- read.csv("output/bp_climatedatapre.csv")
+#climatedatapost <- read.csv("/n/wolkovich_lab/Lab/Cat/bp_climatedatapost.csv")
+climatedatapost <- read.csv("output/bp_climatedatapost.csv")
+#climatedatamid <- read.csv("/n/wolkovich_lab/Lab/Cat/bp_climatedatamid.csv")
 #climatedatamid <- read.csv("output/bp_climatedatamid.csv")
   
-source("/n/wolkovich_lab/Lab/Cat/Run_SW.R")
+source("simmonds_slidingwin/Run_SW.R")
 # refday = c(day, mon)
 # climate is a datafile that must include col = temp
 # datafile = biological data
@@ -106,8 +106,8 @@ climate <- climatedatapre
 climate$X <- NA ### needed in order to run... 
 
 Results_SWRpre <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
-write.csv(Results_SWRpre[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swapre_bp_mayref.csv")
-write.csv(Results_SWRpre[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swapre_bp_mayref.csv")
+write.csv(Results_SWRpre[[2]], file="simmonds_slidingwin/output/results_swapre_bp_mayref.csv")
+write.csv(Results_SWRpre[[1]], file="simmonds_slidingwin/output/sumstats_swapre_bp_mayref.csv")
 
 ### Now checking Simmond's sliding window approach:
 #refday <- c(01, 05) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
@@ -116,9 +116,10 @@ climate <- climatedatapost
 climate$X <- NA ### needed in order to run... 
 
 Results_SWRpost <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
-write.csv(Results_SWRpost[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swapost_bp_mayref.csv")
-write.csv(Results_SWRpost[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swapost_bp_mayref.csv")
+write.csv(Results_SWRpost[[2]], file="simmonds_slidingwin/output/results_swapost_bp_mayref.csv")
+write.csv(Results_SWRpost[[1]], file="simmonds_slidingwin/output/sumstats_swapost_bp_mayref.csv")
 
+if(FALSE){
 ### Now checking Simmond's sliding window approach:
 #refday <- c(01, 04) ### results in folders are from a ref day of 01-03, I think this new ref day is more appropriate for PEP leafout data - to rerun
 datafile <- bbswmid
@@ -128,6 +129,7 @@ climate$X <- NA ### needed in order to run...
 Results_SWRmid <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a long time to run
 write.csv(Results_SWRmid[[2]], file="/n/wolkovich_lab/Lab/Cat/results_swamid_bp_mayref.csv")
 write.csv(Results_SWRmid[[1]], file="/n/wolkovich_lab/Lab/Cat/sumstats_swamid_bp_mayref.csv")
+}
 
 if(FALSE){
 ## Get data and parameters for prediction
